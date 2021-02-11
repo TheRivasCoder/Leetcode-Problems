6d3fe1cf717d8f39865ab0821ac352b189ed659b
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-10 16:45:58 -0800

    added comments

--- Unique Email Addresses.py
+++ Unique Email Addresses.py
@@ -1,3 +1,4 @@
+# List of strings to be passed through function as test
 myEmails = ["test.email+alex@leetcode.com",
 "test.e.mail+bob.cathy@leetcode.com",
 "testemail+david@lee.tcode.com",
@@ -6,18 +7,18 @@ myEmails = ["test.email+alex@leetcode.com",
 "test+emaildavid@leetcodecom"]
 
 class Solution:
-    def numUniqueEmails(emails):
+    def numUniqueEmails(emails): # Takes list of emails and return number of unique emails
         
-        new_email_list = set()
+        new_email_set = set() # create set to reamove duplicates of unique emails when added to new_email_set
         
         for email in emails:
-            name, domain = email.split('@')
-            name = name.replace('.', '')
-            name = name.split('+')[0]
-            new_email = ''.join(name + '@' + domain)
-            new_email_list.add(new_email)
+            name, domain = email.split('@') # split email into name and domain name
+            name = name.replace('.', '') # removes periods from name
+            name = name.split('+')[0] # removes characters after '+' symbol in name
+            new_email = ''.join(name + '@' + domain) # joins name and domain name into new email with '@' symbol in the middle
+            new_email_set.add(new_email) # add new email to new_email_set
             
-        return len(new_email_list)
+        return len(new_email_set) # returns number of unique emails in set
 
 
 print(Solution.numUniqueEmails(myEmails))
