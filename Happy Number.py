9414031f9948c94269d5331637713ca13f3d3602
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-18 17:56:28 -0800

    added comments

--- Happy Number.py
+++ Happy Number.py
@@ -1,18 +1,16 @@
-		
-
 
 class Solution:
     def isHappy(self, n: int) -> bool:       
         visited = set()
-        while n!=1:
+        while n!=1: # create loop to check if n ever becomes 1
             sum = 0
             n = str(n)
             for char in n:
-                sum += int(char)**2
-            if sum in visited:
+                sum += int(char)**2 # squares all digits in n and adds them to sum
+            if sum in visited: # check if new sum has been encountered before to see if we are in an endless loop
                 return False
             visited.add(sum)
-            n = sum
+            n = sum # changes n to new sum to check again if it is equal to 1 and we can return True
         return True
 
 check_if_happy = Solution()
