653278d6c590e7d450c8f2184b5eb483024c9f32
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-20 13:54:22 -0800

    Changed variable names

--- Decode ways.py
+++ Decode ways.py
@@ -4,16 +4,14 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         
         
-        dp = [0]*(len(s)+1)
-        dp[0] = 1
+        variations = [0]*(len(s)+1)
+        variations[0] = 1
         for i in range(len(s)+1):
-            if s[i-1] != "0":
-                dp[i] += dp[i-1]
-            if i != 1 and "09"< s[i-2:i] < "27" : 
-                # print(s[i-2:i])
-                dp[i] += dp[i-2]
-        print(dp)
-        return dp[-1]
+            if s[i-1] != '0':
+                variations[i] += variations[i-1]
+            if i != 1 and '09' < s[i-2:i] < '27':
+                variations[i] += variations[i-2]
+        return variations[-1]
 
 test = Solution()
 
