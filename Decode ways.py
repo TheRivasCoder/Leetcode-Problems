dc9ca4493c7d4c9a70db4cebf2dd037ab0748976
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-20 13:46:11 -0800

    Changed Test case

--- Decode ways.py
+++ Decode ways.py
@@ -1,4 +1,4 @@
-number = '1324223410'
+number = '13242234109'
 
 class Solution:
     def numDecodings(self, s: str) -> int:
@@ -8,11 +8,11 @@ class Solution:
         dp[0] = 1
         for i in range(len(s)+1):
             if s[i-1] != "0":
-                print(s[i-1])
                 dp[i] += dp[i-1]
-            if i != 1 and "09"< s[i-2:i] < "27" :  #"01"ways = 0
-                print(s[i-2:i])
+            if i != 1 and "09"< s[i-2:i] < "27" : 
+                # print(s[i-2:i])
                 dp[i] += dp[i-2]
+        print(dp)
         return dp[-1]
 
 test = Solution()
