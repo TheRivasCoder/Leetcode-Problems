48d197d9c43f9665d318a9c92b419276ebffc5f6
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-21 15:08:50 -0800

    Remove class and extra spaces

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -1,21 +1,17 @@
 t = "abba"
 
+def lengthOfLongestSubstring(s: str) -> int:
+    dic = {}
+    longest = 0
+    start = 0
+    for i in range(len(s)):
+        if s[i] not in dic:
+            dic[s[i]] = i
+        else:
+            start = dic[s[i]] +1
+            dic[s[i]] = i
+        longest = max(longest, i-start+1)
+    return longest
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        dic = {}
-        longest = 0
-        start = 0
-        for i in range(len(s)):
-            if s[i] not in dic:
-                dic[s[i]] = i
-            else:
-                start = dic[s[i]] +1
-                dic[s[i]] = i
-            longest = max(longest, i-start+1)
-        return longest
 
-test = Solution()
-
-print(test.lengthOfLongestSubstring(t))
+print(lengthOfLongestSubstring(t))
