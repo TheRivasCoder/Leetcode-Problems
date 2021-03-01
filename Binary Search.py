a40c489a35fa40d0bf5a1858ec7fec7c4410f558
Chris Rivas <chrisrivasstudio@gmail.com>
2021-02-28 19:14:27 -0800

    Add test case

--- Binary Search.py
+++ Binary Search.py
@@ -1,18 +1,20 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        Left = 0
-        Right = len(nums)-1
-        while Left<=Right:
-            middle = (Left+Right)//2
-            if nums[middle] < target:
-                Left = middle +1
-            elif nums[middle] > target:
-                Right = middle -1
-            elif nums[middle] == target:
-                return middle
-        return -1
+numbers = [-1,0,3,5,9,12]
+target = 13
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    Left = 0
+    Right = len(nums)-1
+    while Left<=Right:
+        middle = (Left+Right)//2
+        if nums[middle] < target:
+            Left = middle +1
+        elif nums[middle] > target:
+            Right = middle -1
+        elif nums[middle] == target:
+            return middle
+    return -1
+print(search(numbers, target))
