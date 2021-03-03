294e0da7eb57960701af12048485483626a93127
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-02 17:44:38 -0800

    Remove class

--- Best Time To Buy and Sell Stock.py
+++ Best Time To Buy and Sell Stock.py
@@ -1,15 +1,18 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        if not prices:
-            return 0
-        max_profit = 0
-        min_price = prices[0]
-        for i in range(1,len(prices)):
-            min_price = min(prices[i], min_price)
-            current = prices[i] - min_price
-            max_profit = max(max_profit, current)
-        return max_profit
+
+Price_list = [4,8,1,23,6,5,2,7,3,8]
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    if not prices:
+        return 0
+    max_profit = 0
+    min_price = prices[0]
+    for i in range(1,len(prices)):
+        min_price = min(prices[i], min_price)
+        current = prices[i] - min_price
+        max_profit = max(max_profit, current)
+    return max_profit
+
