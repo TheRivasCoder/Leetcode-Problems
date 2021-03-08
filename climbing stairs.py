b14ade3f5a023f6728942d2c47e1747b24197fa9
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-07 23:15:13 -0800

    Add climbing stairs problem

---
+++ climbing stairs.py
@@ -0,0 +1,15 @@
+n = 6
+
+def climbStairs(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    prev = 1
+    curr = 2
+    for i in range(2,n):
+        temp = curr
+        curr = prev + curr
+        prev = temp
+    return curr
+print(climbStairs(n))
