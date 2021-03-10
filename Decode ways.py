0b3c9091c42c7a289056a2fc11a358504acb414c
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-09 22:26:52 -0800

    Remove class from test case

--- Decode ways.py
+++ Decode ways.py
@@ -1,19 +1,17 @@
 number = '13242234109'
 
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        
-        
-        variations = [0]*(len(s)+1)
-        variations[0] = 1
-        for i in range(len(s)+1):
-            if s[i-1] != '0':
-                variations[i] += variations[i-1]
-            if i != 1 and '09' < s[i-2:i] < '27':
-                variations[i] += variations[i-2]
-        return variations[-1]
 
-test = Solution()
+def numDecodings(s):
+    
+    
+    variations = [0]*(len(s)+1)
+    variations[0] = 1
+    for i in range(len(s)+1):
+        if s[i-1] != '0':
+            variations[i] += variations[i-1]
+        if i != 1 and '09' < s[i-2:i] < '27':
+            variations[i] += variations[i-2]
+    return variations[-1]
 
-print(test.numDecodings(number))
+print(numDecodings(number))
         
