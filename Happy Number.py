21411afde1eedd88fffca3cb856c00051c0617e9
Chris Rivas <chrisrivasstudio@gmail.com>
2021-03-10 23:54:37 -0800

    Remove class from function

--- Happy Number.py
+++ Happy Number.py
@@ -1,23 +1,23 @@
 
-class Solution:
-    def isHappy(self, n: int) -> bool:       
-        visited = set()
-        while n!=1: # create loop to check if n ever becomes 1
-            sum = 0
-            n = str(n)
-            for char in n:
-                sum += int(char)**2 # squares all digits in n and adds them to sum
-            if sum in visited: # check if new sum has been encountered before to see if we are in an endless loop
-                return False
-            visited.add(sum)
-            n = sum # changes n to new sum to check again if it is equal to 1 and we can return True
-        return True
 
-check_if_happy = Solution()
+def isHappy(n):      
+    visited = set()
+    while n!=1: # create loop to check if n ever becomes 1
+        sum = 0
+        n = str(n)
+        for char in n:
+            sum += int(char)**2 # squares all digits in n and adds them to sum
+        if sum in visited: # check if new sum has been encountered before to see if we are in an endless loop
+            return False
+        visited.add(sum)
+        n = sum # changes n to new sum to check again if it is equal to 1 and we can return True
+    return True
 
-print(check_if_happy.isHappy(2))
-print(check_if_happy.isHappy(3))
-print(check_if_happy.isHappy(4))
-print(check_if_happy.isHappy(5))
-print(check_if_happy.isHappy(6))
-print(check_if_happy.isHappy(7))
+
+
+print(isHappy(2))
+print(isHappy(3))
+print(isHappy(4))
+print(isHappy(5))
+print(isHappy(6))
+print(isHappy(7))
