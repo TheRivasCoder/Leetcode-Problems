b183dd273be6e8136bf332a90f91a946b0c9f97a
Chris Rivas <chrisrivasstudio@gmail.com>
2021-04-03 13:56:51 -0700

    Change solution

--- Decode ways.py
+++ Decode ways.py
@@ -1,17 +1,24 @@
-number = '13242234109'
 
 
-def numDecodings(s):
-    
-    
-    variations = [0]*(len(s)+1)
-    variations[0] = 1
-    for i in range(len(s)+1):
-        if s[i-1] != '0':
-            variations[i] += variations[i-1]
-        if i != 1 and '09' < s[i-2:i] < '27':
-            variations[i] += variations[i-2]
-    return variations[-1]
 
-print(numDecodings(number))
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        return self.helper(0, s)
+
+    def helper(self, index, s):
+        
+        # If you reach the end of the string
+        # Return 1 for success.
+        if index == len(s):
+            return 1
+        # If the string starts with a zero, it can't be decoded
+        if s[index] == '0':
+            return 0
+        if index == len(s)-1:
+            return 1
+        
+        answer = self.helper(index + 1, s)
+        if int(s[index : index + 2]) <= 26:
+            answer += self.helper(index + 2, s)
+        return answer
         
